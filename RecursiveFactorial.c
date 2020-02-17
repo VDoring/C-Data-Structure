1eb306678780b4839f8c54ed68a197349324b1fc
Doring <hojun.dev@gmail.com>
2020-02-17 22:07:07 +0900

    재귀함수 이용한 펙토리얼

---
+++ RecursiveFactorial.c
@@ -0,0 +1,19 @@
+//55p
+#include <stdio.h>
+
+int Factorial(int n) {
+	if (n == 0)
+		return 1;
+	else
+		return n * Factorial(n - 1);//ex) 만약 n=1일경우 1 * 0 = 1 (확인필요) 
+									//ex) 만약 n=2일경우 2 * 1 = 2
+									//ex) 만약 n=3일경우 3 * 2 = 6
+}
+
+int main() {
+	printf("1! = %d \n", Factorial(1));
+	printf("2! = %d \n", Factorial(2));
+	printf("3! = %d \n", Factorial(3));
+	printf("4! = %d \n", Factorial(4));
+	printf("9! = %d \n", Factorial(9));
+}
