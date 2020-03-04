06ec31da71d165ad81c4e30b901d86c707cfa3c7
Doring <hojun.dev@gmail.com>
2020-03-04 14:17:23 +0900

    배열 기반 리스트

---
+++ Linked List/ListMain.c
@@ -0,0 +1,50 @@
+//81p
+#include <stdio.h>
+#include "Arraylist.h"
+
+int main() {
+	//Arraylist의 생성 및 초기화 //////
+	List list;  //리스트 생성
+	int data;
+	ListInit(&list); //리스트 초기화
+
+	//5개의 데이터 저장 //////
+	LInsert(&list, 11); LInsert(&list, 11);
+	LInsert(&list, 22); LInsert(&list, 22);
+	LInsert(&list, 33);
+
+	//저장된 데이터의 전체 출력 //////
+	printf("현재 데이터의 수: %d \n", LCount(&list));
+
+	if (LFirst(&list, &data)) { //첫번쨰 데이터 조회
+		printf("%d ", data);
+
+		while (LNext(&list, &data))
+			printf("%d ", data);
+	}
+	printf("\n\n");
+
+	//숫자 22을 탐색하여 모두 삭제 //////
+	if (LFirst(&list, &data)) {
+		if (data == 22)
+			LRemove(&list);
+
+		while (LNext(&list, &data)) {
+			if (data == 22)
+				LRemove(&list);
+
+		}
+	}
+
+	//삭제 후 남은 데이터 전체 출력 //////
+	printf("현재 데이터의 수: %d \n", LCount(&list));
+
+	if (LFirst(&list, &data)) {
+		printf("%d ", data);
+
+		while (LNext(&list, &data))
+			printf("%d ", data);
+	}
+	printf("\n\n");
+	return 0;
+}
