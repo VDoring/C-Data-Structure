128ca5b49e27f1819414f21c5786eff2ec6a26ec
Doring <hojun.dev@gmail.com>
2020-07-01 17:46:22 +0900

    더미 노드(Dummy Node) 기반의 단순 연결 리스트 구현: 하나로 묶기

---
+++ Linked List 2/DLinkedList.c
@@ -0,0 +1,71 @@
+//139p
+#include <stdio.h>
+#include <stdlib.h>
+#include "DLinkedList.h"
+
+void ListInit(List * plist)
+{
+	plist->head = (Node*)malloc(sizeof(Node));
+	plist->head->next = NULL;
+	plist->comp = NULL;
+	plist->numOfData = 0;
+}
+
+void FInsert(List * plist, LData data)
+{
+	Node * newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+	
+	newNode->next = plist->head->next;
+	plist->head->next = newNode;
+	
+	(plist->numOfData)++;
+}
+
+void SInsert(List * plist, LData data)
+{
+	//보류
+}
+
+void LInsert(List * plist, LData data)
+{
+	if(plist->comp == NULL)
+		FInsert(plist, data);
+	else
+		SInsert(plist, data);
+}
+
+int LFirst(List * plist, LData * pdata)
+{
+	if(plist->head->next == NULL)
+		return FALSE;
+	
+	plist->before = plist->cur;
+	plist->cur = plist->cur->next;
+	
+	*pdata = plist->cur->data;
+	return TRUE;
+}
+
+LData LRemove(List * plist)
+{
+	Node * rpos = plist->cur;
+	LData rdata = rpos->data;
+	
+	plist->before->next = plist->cur->next;
+	plist->cur = plist->before;
+	
+	free(rpos);
+	(plist->numOfData)--;
+	return rdata;
+}
+
+int LCount(List * plist)
+{
+	return plist->numOfData;
+}
+
+void SetSortRule(List * plist, int (*comp)(LData d1, LData d2))
+{
+	//보류
+}
