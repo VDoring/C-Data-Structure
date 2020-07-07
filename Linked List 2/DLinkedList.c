6b733afa67ed667814807e1a830cb1bb35e0e2e4
Doring <hojun.dev@gmail.com>
2020-07-07 22:00:28 +0900

    더미 노드(Dummy Node) 기반의 단순 연결 리스트 구현: 하나로 묶기

--- Linked List 2/DLinkedList.c
+++ Linked List 2/DLinkedList.c
@@ -5,7 +5,7 @@
 
 void ListInit(List * plist)
 {
-	plist->head = (Node*)malloc(sizeof(Node));
+	plist->head = (Node*)malloc(sizeof(Node)); //더미 노드의 생성 (head는 Dummy Node를 가리킨다)
 	plist->head->next = NULL;
 	plist->comp = NULL;
 	plist->numOfData = 0;
@@ -13,13 +13,13 @@ void ListInit(List * plist)
 
 void FInsert(List * plist, LData data)
 {
-	Node * newNode = (Node*)malloc(sizeof(Node));
-	newNode->data = data;
+	Node * newNode = (Node*)malloc(sizeof(Node)); //새 노드 생성
+	newNode->data = data; //새 노드에 데이터 저장
 	
-	newNode->next = plist->head->next;
-	plist->head->next = newNode;
+	newNode->next = plist->head->next; //새 노드가 다른 노드를 가리키게 함
+	plist->head->next = newNode; //더미 노드가 새 노드를 가리키게 함
 	
-	(plist->numOfData)++;
+	(plist->numOfData)++; //저장된 노드의 수를 하나 증가시킴
 }
 
 void SInsert(List * plist, LData data)
@@ -29,35 +29,47 @@ void SInsert(List * plist, LData data)
 
 void LInsert(List * plist, LData data)
 {
-	if(plist->comp == NULL)
-		FInsert(plist, data);
-	else
-		SInsert(plist, data);
+	if(plist->comp == NULL) //정렬기준이 마련되지 않았다면,
+		FInsert(plist, data); //머리에 노드를 추가!
+	else //정렬기준이 마련되었다면,
+		SInsert(plist, data); //정렬기준에 근거하여 노드를 추가!
 }
 
 int LFirst(List * plist, LData * pdata)
 {
-	if(plist->head->next == NULL)
-		return FALSE;
+	if(plist->head->next == NULL) //더미 노드가 NULL을 가리킨다면,
+		return FALSE; //반환할 데이터가 없다!
 	
-	plist->before = plist->cur;
-	plist->cur = plist->cur->next;
+	plist->before = plist->cur; //before는 더미 노드를 가리키게 함
+	plist->cur = plist->cur->next; //cur은 첫 번째 노드를 가리키게 함
 	
-	*pdata = plist->cur->data;
-	return TRUE;
+	*pdata = plist->cur->data; //첫 번쨰 노드의 데이터를 전달
+	return TRUE; //데이터 반환 성공!
+}
+
+int LNext(List* plist, LData* pdata)
+{
+	if (plist->cur->next == NULL) //cur이 NULL을 가리킨다면,
+		return FALSE; //반환할 데이터가 없다!
+
+	plist->before = plist->cur; //cur이 가리키던 것을 before가 가리킴
+	plist->comp = plist->cur->next; //cur은 그 다음 노드를 가리킴
+
+	*pdata = plist->cur->data; //cur이 가리키는 노드의 데이터 전달
+	return TRUE; //데이터 반환 성공!
 }
 
 LData LRemove(List * plist)
 {
-	Node * rpos = plist->cur;
-	LData rdata = rpos->data;
+	Node * rpos = plist->cur; //소멸 대상의 주소 값을 rpos에 저장
+	LData rdata = rpos->data; //소멸 대상의 데이터를 rdata에 저장
 	
-	plist->before->next = plist->cur->next;
-	plist->cur = plist->before;
+	plist->before->next = plist->cur->next; //소멸 대상을 리스트에서 제거
+	plist->cur = plist->before; //cur이 가리키는 위치를 재조정!
 	
-	free(rpos);
-	(plist->numOfData)--;
-	return rdata;
+	free(rpos); //리스트에서 제거된 노드 소멸
+	(plist->numOfData)--; //저장된 데이터의 수 하나 감소
+	return rdata; //제거된 노드의 데이터 반환
 }
 
 int LCount(List * plist)
